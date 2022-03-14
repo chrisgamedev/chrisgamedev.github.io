872785be157ebe0491258ede67e97c60fb672a39
timeBenter <timeBenter@gmail.com>
2022-03-14 13:48:37 +1000

    update
    - fixed stuff and branches

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './css/Footer.css'
+import 'font-awesome/css/font-awesome.min.css';
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <div className="footer">
+                <p>
+                    <a href="https://twitter.com/timeBenter" className="fa fa-twitter"></a>
+                    <a href="mailto:chris.gamedev0@gmail.com?subject=Issue Report&body=Please provide a detailed report of your issue." className="fa fa-envelope"></a>
+                </p>
+            </div>
+        )
+    }
+}
---
+++ src/pages/Tutorials.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+import Panel from '../components/Panel'
+import Row from '../components/Row'
+
+export default class Tutorials extends Component {
+    render() {
+        return (
+            <div>                
+                <p>Check out all of my tutorials! If you find a mistake somewhere, let me know.</p>
+
+                <Row>
+                    <Panel href="/tutorials/how-to-develop-a-unity-app-for-ios-on-windows" title={"How to develop a Unity App for iOS on Windows"}/>
+                    <Panel href="/tutorials/how-to-make-a-drawing-game-in-unity" title={"How to make a drawing / painting game in Unity"}/>
+                </Row>
+
+                <p>More to come!</p>
+            </div>
+        )
+    }
+}
